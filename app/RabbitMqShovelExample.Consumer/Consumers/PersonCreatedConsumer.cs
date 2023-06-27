30fff0fc6c4bc43e257683be60e14123bffd0529
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2023-06-27 19:36:30 +0100

    Resolve / ignore warnings

--- app/RabbitMqShovelExample.Consumer/Consumers/PersonCreatedConsumer.cs
+++ app/RabbitMqShovelExample.Consumer/Consumers/PersonCreatedConsumer.cs
@@ -7,16 +7,20 @@ namespace RabbitMqShovelExample.Consumer.Consumers
     public class PersonCreatedConsumer : IConsumer<PersonCreatedEvent>
     {
         private readonly ILogger<PersonCreatedConsumer> _logger;
+        private readonly bool _badConsumer;
 
-        public PersonCreatedConsumer(ILogger<PersonCreatedConsumer> logger)
+        public PersonCreatedConsumer(ILogger<PersonCreatedConsumer> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _badConsumer = configuration.GetValue<bool>("AppSettings:BadConsumer");
         }
 
         public Task Consume(ConsumeContext<PersonCreatedEvent> context)
         {
-            // Comment this line out to consume the messages successfully
-            throw new NotImplementedException();
+            if (_badConsumer)
+            {
+                throw new Exception($"Intentional exception consuming message {JsonConvert.SerializeObject(context.Message)}");
+            }
 
             _logger.LogInformation($"Consumed message: {JsonConvert.SerializeObject(context.Message)}");
             return Task.CompletedTask;
